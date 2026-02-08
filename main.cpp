890fdf20002f69a44e8f7ec3d6b156bd8212e744
ChronoXEN <140365988+ChronoXEN@users.noreply.github.com>
2026-02-09 05:03:52 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,36 +2,33 @@
 #include <mod/logger.h>
 #include <mod/config.h>
 #include <jni.h>
-#include <stdlib.h> // For abort()
+#include <stdlib.h>
+
+// --- 1. FORCE VISIBILITY MACROS ---
+// These tell the compiler "Keep these visible to the outside world!"
+#define EXPORT __attribute__((visibility("default")))
 
 IAMLer* aml = new IAMLer();
+
+// We also mark the config as 'used' so it doesn't get optimized away
 MYMODCFG(net.rusjj.legik, LegIK Mod, 1.0, YourName)
 
 // Symbol we are testing
 void* (*RpAnimBlendClumpFindBone)(void* clump, unsigned int boneID) = nullptr;
 
-extern "C" void OnModLoad() {
-    // 1. GET HANDLE
+// --- 2. THE EXPORTED FUNCTION ---
+// Notice the 'EXPORT' tag. This is the fix.
+extern "C" EXPORT void OnModLoad() {
+    
+    // ---------------------------------------------------------
+    // THE CRASH TEST (ROUND 2)
+    // ---------------------------------------------------------
+    
+    // Uncomment this line to force a crash.
+    // If the game crashes, the mod is finally loading!
+    abort(); 
+
+    // Standard Setup (This runs if you comment out abort)
     void* hGame = aml->GetLibHandle("libGTASA.so");
-    
-    // 2. CHECK FOR THE SYMBOL YOU FOUND
-    // If this symbol exists in v2.00, this pointer will be valid.
-    // If it doesn't exist, this pointer will be NULL.
     RpAnimBlendClumpFindBone = (void*(*)(void*, unsigned int))aml->GetSym(hGame, "_Z24RpAnimBlendClumpFindBoneP7RpClumpj");
-
-    // ---------------------------------------------------------
-    // THE CRASH TEST
-    // ---------------------------------------------------------
-    
-    // TEST A: PROVE MOD IS LOADED
-    // Uncomment the line below. If the game crashes instantly, your mod is installed correctly.
-    // abort(); 
-
-    // TEST B: PROVE SYMBOL EXISTS
-    // If the symbol was NOT found, we crash the game to tell you.
-    if (RpAnimBlendClumpFindBone == nullptr) {
-        // FORCE CRASH by writing to null pointer
-        // This means: "I am loaded, but I can't find the function!"
-        *(int*)0 = 0; 
-    }
 }
